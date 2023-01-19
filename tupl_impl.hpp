6b8c4cfdbc3448d1c97abdfabd8e8607d3c0d52b
Will Wray <will@lemurianlabs.com>
2023-01-19 13:33:48 -0400

    tupl_impl commit; forgot again

--- tupl_impl.hpp
+++ tupl_impl.hpp
@@ -714,6 +714,18 @@ struct assign_to<Lr>
       });
     return l;
   }
+  constexpr value_type& operator=(tupl<L const&> r) const
+    noexcept(std::is_nothrow_copy_assignable_v<L>)
+    requires (tupl_val<L> && std::is_copy_assignable_v<L>)
+  {
+    return l = r;
+  }
+  constexpr value_type& operator=(tupl<L&&> r) const
+    noexcept(std::is_nothrow_move_assignable_v<L>)
+    requires (tupl_val<L> && std::is_move_assignable_v<L>)
+  {
+    return l = (L&&)r;
+  }
   // assign from other tuplish
   template <tuplish R>
   constexpr value_type& operator=(R&& r) const
