c87f7bab858453f294a2bf8c602150a670f721d6
Will Wray <will@lemurianlabs.com>
2023-01-20 13:48:50 -0400

    Fix tupl assign same type lval, remove const

--- tupl_impl.hpp
+++ tupl_impl.hpp
@@ -714,7 +714,7 @@ struct assign_to<Lr>
       });
     return l;
   }
-  constexpr value_type& operator=(L const& r) const
+  constexpr value_type& operator=(L& r) const
     noexcept(std::is_nothrow_copy_assignable_v<L>)
     requires (tupl_val<L> && std::is_copy_assignable_v<L>)
   {
